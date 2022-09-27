59bc3177bf4725fd06256be723d15a86a14e157b
Evgeniy <shiryaev.ep@gmail.com>
2022-09-27 09:45:37 +0300

    homework4 done

---
+++ task10_29/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 29: Напишите программу, в которой пользователь задает длину массива, элементы которого задаются в диапазоне [1,99] и выводит на экран .
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]
+*/
+int lenArray = ReadInt("Введите количество чисел массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,99);
+    Console.Write(randomArray[i] + " ");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
