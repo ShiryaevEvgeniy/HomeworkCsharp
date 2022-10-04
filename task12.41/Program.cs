db8673b11b135b610e6f3f6e0739411f22f9477b
Evgeniy <shiryaev.ep@gmail.com>
2022-10-04 10:16:11 +0300

    homework6 done

---
+++ task12.41/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+Console.Write($"Введи число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[m];
+
+//Создание списка чисел
+
+void InputNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+//Сравнение по индексу
+
+int Comparison(int[] numbers)
+{
+  int count = 0;
+  for (int i = 0; i < numbers.Length; i++)
+  {
+    if(numbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+//вывод решения
+
+InputNumbers(m);
+Console.WriteLine($"Введено чисел больше 0: {Comparison(numbers)} ");
---
+++ task12.43/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+double[,]coeff = new double[2, 2];
+double[] cross = new double[2];
+
+//Ввод коэфициентов
+
+void InputCoeff()
+{
+  for (int i = 0; i < coeff.GetLength(0); i++)
+  {
+    Console.Write($"Введите углвые коэффициенты для уравнения (y = k * x + b):\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите угловой коэффициент k: ");
+      else Console.Write($"Введите угловой коэффициент b: ");
+      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+// Решение задачи 
+
+double[] Findingcross(double[,] coeff)
+
+{
+  cross[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  cross[1] = cross[0] * coeff[0,0] + coeff[0,1];
+  return cross;
+}
+
+//Вывод результата
+
+void Output(double[,] coeff)
+{
+  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
+  {
+    Console.Write($"\n Прямые совпадают, у нас бесконечно много решений");
+  }
+  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
+  {
+    Console.Write($"\n Прямые параллельны, решения нет");
+  }
+  else 
+  {
+    Findingcross(coeff);
+    Console.Write($"\n Точка пересечения прямых: ({cross[0]}, {cross[1]})");
+  }
+}
+InputCoeff();
+Output(coeff);
